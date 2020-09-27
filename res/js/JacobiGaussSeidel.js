907feae1d396e0c8c5b675d8e334fe067120c167
Luis David <luisdtorresp@gmail.com>
2020-09-27 19:17:15 -0400

    feat:Add Input fields created dynamically, Clean Global Variables
    -Catch data from input fields.
    -Pending: Initial Vector input.
    -TODO: Validate input, fix UI.

--- res/js/JacobiGaussSeidel.js
+++ res/js/JacobiGaussSeidel.js
@@ -1,72 +1,115 @@
-var textJacobi = document.getElementById("jacobi")
-
-//================ Variables ====================
-
-numEcuaciones = 4;
-/*
-matrizA =  [[ 6, 3,-1,-2],              // Ordenada por Fila
-            [-3, 9,-1, 3],
-            [ 2,-1,13,-1],
-            [ 5,-1, 2, 8]
-            ];
-
-//*/
-matrizA =  [[-3, 9,-1, 3],              // ORIGINAL
-            [ 2,-1,13,-1],
-            [ 5,-1, 2, 8],
-            [ 6, 3,-1,-2]];
-/*
-matrizA =  [[-3, 9,-1, 3],              // TESTEO
-            [ 2,-1,13,-1],
-            [ 8, 5,-1, 2],
-            [ 6, 3,-1,-2]];
 
 
-//
-matrizA =  [[  9, -1, 3,-3],            // Ordenada por Columna
-            [ -1, 13,-1, 2],
-            [ -1,  2, 8, 5],
-            [  3, -1,-2, 6]];
-*/
-//  Solucion para Matrices de Orden n  
-/*
-matrizADinamica = []
-matrizADinamica.length
-
-function crearMatrizCuadrada( numEcuacionesDin )
-if (numEcuacionesDin >= 2 && numEcuacionesDin <= 50) { 
-    matrizADinamica = new Array(numEcuacionesDin)
-    for (let filaDinamica = 0; filaDinamica < matrizADinamica.length; filaDinamica++){        
-        for (let columnaDinamica = 0; columnaDinamica < matrizADinamica.length; columnaDinamica++){
-            matrizADinamica[filaDinamica] = new Array(numEcuacionesDin)
+function crearMatrizCuadrada( numEcuacionesDin ){
+    if (numEcuacionesDin >= 2 && numEcuacionesDin <= 50) { 
+        var matrizADinamica = new Array(numEcuacionesDin)
+        for (let filaDinamica = 0; filaDinamica < matrizADinamica.length; filaDinamica++){        
+            for (let columnaDinamica = 0; columnaDinamica < matrizADinamica.length; columnaDinamica++){
+                matrizADinamica[filaDinamica] = new Array(numEcuacionesDin)
+            }
         }
+
+        for(let i = 0; i < numEcuacionesDin; i++){
+            for (let j = 0; j < numEcuacionesDin; j++){
+                let celda = document.getElementById(`celdaA${i+1}${j+1}`)
+                if (celda.value == "" || celda.value == null || celda.value == undefined){
+                    celda.value = 0;                    
+                } else {
+                    try {
+                        valorcelda = eval(celda.value)                              // CORREGIR VALIDACION !!!
+
+                    } catch (error) {
+                        console.error("Se genero un error:" + error)
+                        alert("error: "+ error)
+                    }
+                }
+                matrizADinamica[i][j] = parseFloat(celda.value)
+            }
+        }
+        return matrizADinamica;
+    
+    } else {
+        console.log("No se pudo crear la matriz. Tamaño Invalido")
     }
-    return matrizADinamica;
-
-} else {
-    console.log("No se pudo crear la matriz. Tamaño Invalido")
 }
-*/
+
+function crearMatrizB(numEcuacionesDin){
+    if (numEcuacionesDin >= 2 && numEcuacionesDin <= 50) { 
+        var matrizBDinamica = new Array(numEcuacionesDin)
+
+        for (let fila = 0; fila < numEcuacionesDin; fila++){
+            let celda = document.getElementById(`celdaB${fila+1}`)
+                if (celda.value == "" || celda.value == null || celda.value == undefined){
+                    celda.value = 0;                    
+                } else {
+                    try {
+                        valorcelda = eval(celda.value)                              // CORREGIR VALIDACION !!!
+
+                    } catch (error) {
+                        console.error("Se genero un error:" + error)
+                        alert("error: "+ error)
+                    }
+                }
+                matrizBDinamica[fila] = parseFloat(celda.value)
+        }
+
+        return matrizBDinamica;
+    } else {
+        console.log("No se pudo crear la matriz. Tamaño Invalido")
+        return []
+    }    
+}
+
+function crearInputMatriz(){
+
+    var inputNumEc = document.getElementById("numecuaciones");
+    var numEcuaciones = parseInt(inputNumEc.value);
+
+    var divMatrizA = document.getElementById("matrizA");
+    divMatrizA.innerHTML = ""
+    for (let i = 1; i <= numEcuaciones; i++){
+        
+        let uncFila = "\\u208"+ i.toString()
+
+        let filaMatriz = document.createElement("div");
+        filaMatriz.className = "control has-text-centered";
+
+        for (let j = 1; j <= numEcuaciones; j++){
+
+           
+            uncColumna = "\\u208"+ j.toString()
+            
+
+            let celdaCoef = document.createElement("input");
+            celdaCoef.className = "input is-2 has-text-centered";
+            celdaCoef.setAttribute("type","text");
+            celdaCoef.setAttribute("id",`celdaA${i}${j}`)
+            celdaCoef.setAttribute("placeholder", `a${eval("'"+uncFila+"'")}${eval("'"+uncColumna+"'")}`);
+            //celdaCoef.setAttribute("value","0");
+            filaMatriz.appendChild(celdaCoef);
+
+        }
+
+        let celdaMatrizB = document.createElement("input");
+        celdaMatrizB.className = "input ml-5 has-text-centered";
+        celdaMatrizB.setAttribute("id",`celdaB${i}`)
+        celdaMatrizB.setAttribute("placeholder", `b${eval("'"+uncFila+"'")}`);
+        filaMatriz.appendChild(celdaMatrizB);
+
+        divMatrizA.appendChild(filaMatriz);
+
+    }
 
 
-//var vectorXInicial = [0,0,0,0];
+}
 
-var matrizB = [21,-11,-7,5];
-
-//var vectorXResultado = [0,0,0,0];
-
-var maxIteraciones = 200;
-var iteraciones = 0;
-
-//expError
-epsilon = 1e-12;
-
-
-
-function jacobi(){
-  //  var textJacobi = document.getElementById("jacobi")
-    var vectorXInicial = [0,0,0,0];
-    var vectorXResultado = [0,0,0,0];
+function jacobi(numEcuaciones, matrizA, matrizB, vectorXInicial, epsilon, maxIteraciones, numiteramostrar = 4){
+    var textJacobi = document.getElementById("jacobi")
+    var textResultados = document.getElementById("resultadositer")
+    
+    //var vectorXInicial = [0,0,0,0];
+    var vectorXResultado = new Array(numEcuaciones);
+    vectorXResultado = [0]
     var iteraciones = 1;
     while (iteraciones <= maxIteraciones) {
         
@@ -104,7 +147,16 @@ function jacobi(){
             }
         }
         
-        if (iteraciones <= 4){console.log (`Vector Solucion obtenido en ${iteraciones} iteraciones:\n${vectorXResultado.map( x => {return x.toFixed(4)})}`)}
+        if (iteraciones <= numiteramostrar){
+            
+            console.log (`Vector Solucion obtenido en ${iteraciones} iteraciones:\n(${vectorXResultado.map( x => {return x.toFixed(4)})})\n`)
+            if (iteraciones <= 1) textResultados.innerText = "============ METODO JACOBI =============";
+            textResultados.innerText +=`\nIteracion ${iteraciones}:\n(${vectorXResultado.map( x => {return x.toFixed(4)})})\n`
+            /*
+            vectorXResultado.forEach(xres => {
+                textResultados.innerText += "\n" + xres.toFixed(4)    
+            });*/
+        }
         
         vectorXInicial = vectorXResultado.slice();
         iteraciones++;
@@ -125,9 +177,9 @@ function jacobi(){
 }
 
 
-function gaussSeidel(){
+function gaussSeidel(numEcuaciones, matrizA, matrizB, vectorXInicial, epsilon, maxIteraciones){
 
-    var vectorXInicial = [0,0,0,0];
+    //var vectorXInicial = [0,0,0,0];
     var vectorXResultado = [0,0,0,0];
     var iteraciones = 1;
     var textGauss = document.getElementById("gauss")
@@ -196,7 +248,7 @@ function gaussSeidel(){
 }
 
 
-function matrizDominante() {
+function matrizDominante(numEcuaciones, matrizA) {
     let dominante = 0;
     for (let i = 0; i < numEcuaciones; i++){
         
@@ -219,35 +271,7 @@ function matrizDominante() {
 }
 
 
-function ordenarMatrizDominante(){
-
-    indicesAOrdenar = elementosDominante()
-
-    if (indicesUnicos(indicesAOrdenar)){
-        var auxMatrizA = matrizA.slice()
-        var auxMatrizB = matrizB.slice()
-
-        for (let i = 0; i < numEcuaciones; i++){
-            auxMatrizA[indicesAOrdenar[i]] = matrizA[i].slice()
-            auxMatrizB[indicesAOrdenar[i]] = matrizB[i]
-        }
-
-        matrizA = auxMatrizA.slice()
-        matrizB = auxMatrizB.slice()
-
-    } else {
-        alert("La Matriz dada no puede ser ordena dominante por fila")
-        console.log("La Matriz dada no puede ser ordena dominante por fila")
-    }
-
-    
-
-
-}
-
-
-
-function elementosDominante(){
+function elementosDominante(numEcuaciones, matrizA){
 
     var indicesDominantes =  new Array(numEcuaciones)
     //let mayor = -1*Infinity
@@ -286,32 +310,146 @@ function indicesUnicos(arrayIndices){
 }
 
 
+function ordenarMatrizDominante(numEcuaciones, matrizA, matrizB){
+
+    indicesAOrdenar = elementosDominante(numEcuaciones ,matrizA)
+
+    if (indicesUnicos(indicesAOrdenar)){
+        var auxMatrizA = matrizA.slice()
+        var auxMatrizB = matrizB.slice()
+
+        for (let i = 0; i < numEcuaciones; i++){
+            auxMatrizA[indicesAOrdenar[i]] = matrizA[i].slice()
+            auxMatrizB[indicesAOrdenar[i]] = matrizB[i]
+        }
+        /*
+        matrizA = auxMatrizA.slice()
+        matrizB = auxMatrizB.slice()
+        */
+
+        matrices = {
+            mA : auxMatrizA.slice(),
+            mB : auxMatrizB.slice()
+        }
+
+        return matrices
 
 
+    } else {
+        alert("La Matriz dada no puede ser ordena dominante por fila")
+        console.log("La Matriz dada no puede ser ordena dominante por fila")
+    }
 
-
-
-
-if (matrizDominante()) {
     
-    resultadoJacobi = jacobi();
-    console.log(resultadoJacobi)
-    resultadoGauss = gaussSeidel();
-    console.log(resultadoGauss)
 
-} else { 
-    ordenarMatrizDominante()
-    console.log("MATRIZ ORDENADA!\n")
-    resultadoJacobi = jacobi();
-    console.log(resultadoJacobi)
-    resultadoGauss = gaussSeidel();
-    console.log(resultadoGauss)
 
 }
 
 
 
 
+//========================  MAIN  function ============================
+
+crearmatriz = document.getElementById("crearMatriz");
+crearmatriz.addEventListener("click", ()=>{crearInputMatriz();});
+
+calcular = document.getElementById("calcular");
+calcular.addEventListener("click",
+
+() => {
+
+    //  Solucion para Matrices de Orden n  
+    //var matrizADinamica = crearMatrizCuadrada(numEcuaciones)
+
+   // var textJacobi = document.getElementById("jacobi")
+
+    //================ Variables ====================
+
+    var inputNumEc = document.getElementById("numecuaciones");
+    var numeroEcuaciones = parseInt(inputNumEc.value);
+
+   /* matrizAOriginal =  [[-3, 9,-1, 3],              // ORIGINAL
+                        [ 2,-1,13,-1],
+                        [ 5,-1, 2, 8],
+                        [ 6, 3,-1,-2]];
+*/
+
+    var inputVectorInicial = document.getElementById("vectorinicial");
+    var vectorInicial = [0,0,0,0];
+
+   // var matrizSolucion = [21,-11,-7,5];
+
+    //var vectorXResultado = [0,0,0,0];
+
+    var inputmaxIteraciones = document.getElementById("maxiteraciones");
+    var maxIteraciones = parseInt(inputmaxIteraciones.value);
+
+
+    //expError
+    var inputepsilon = document.getElementById("epsilon");
+    var epsilonPrecision = Math.pow(10,parseInt(inputepsilon.value));
+
+    var inputnumiteramostrar = document.getElementById("numiteramostrar");
+    numIteraciones = parseInt(inputnumiteramostrar.value)
+ // ===================================================
+
+    matrizAOriginal = crearMatrizCuadrada(numeroEcuaciones)
+    matrizSolucion = crearMatrizB(numeroEcuaciones)
+
+    if (matrizDominante(numeroEcuaciones,matrizAOriginal)) {
+    
+        resultadoJacobi = jacobi(numeroEcuaciones, matrizAOriginal, matrizSolucion, vectorInicial, epsilonPrecision, maxIteraciones, numIteraciones);
+        console.log(resultadoJacobi)
+        resultadoGauss = gaussSeidel(numeroEcuaciones, matrizAOriginal, matrizSolucion, vectorInicial, epsilonPrecision, maxIteraciones);
+        console.log(resultadoGauss)
+    
+    } else { 
+        matrizOrdenada = ordenarMatrizDominante( numeroEcuaciones, matrizAOriginal, matrizSolucion)
+        console.log("MATRIZ ORDENADA!\n")
+        resultadoJacobi = jacobi(numeroEcuaciones, matrizOrdenada.mA, matrizOrdenada.mB, vectorInicial, epsilonPrecision, maxIteraciones, numIteraciones);
+        console.log(resultadoJacobi)
+        resultadoGauss = gaussSeidel(numeroEcuaciones, matrizOrdenada.mA, matrizOrdenada.mB, vectorInicial, epsilonPrecision, maxIteraciones);
+        console.log(resultadoGauss)
+    
+    }
+    
+/*
+    console.log("============ METODO JACOBI =============")
+    //resultadoJacobi = jacobi();
+    //console.log(resultadoJacobi)
+
+    valor = (-3*resultadoJacobi[0])+(9*resultadoJacobi[1])+(-1*resultadoJacobi[2])+(3*resultadoJacobi[3])
+    console.log("Solucion Ec1 = " + valor)
+
+    valor = (2*resultadoJacobi[0])+(-1*resultadoJacobi[1])+(13*resultadoJacobi[2])+(-1*resultadoJacobi[3])
+    console.log("Solucion Ec2 = " + valor)
+
+    valor = (5*resultadoJacobi[0])+(-1*resultadoJacobi[1])+(2*resultadoJacobi[2])+(8*resultadoJacobi[3])
+    console.log("Solucion Ec3 = " + valor)
+
+    valor = (6*resultadoJacobi[0])+(3*resultadoJacobi[1])+(-1*resultadoJacobi[2])+(-2*resultadoJacobi[3])
+    console.log("Solucion Ec4 = " + valor)
+
+
+    console.log("============ METODO GAUSS-SEIDEL ==============")
+    //resultadoGauss = gaussSeidel();
+    //console.log(resultadoGauss)
+
+    valor = (-3*resultadoGauss[0])+(9*resultadoGauss[1])+(-1*resultadoGauss[2])+(3*resultadoGauss[3])
+    console.log("Solucion Ec1 = " + valor)
+
+    valor = (2*resultadoGauss[0])+(-1*resultadoGauss[1])+(13*resultadoGauss[2])+(-1*resultadoGauss[3])
+    console.log("Solucion Ec2 = " + valor)
+
+    valor = (5*resultadoGauss[0])+(-1*resultadoGauss[1])+(2*resultadoGauss[2])+(8*resultadoGauss[3])
+    console.log("Solucion Ec3 = " + valor)
+
+    valor = (6*resultadoGauss[0])+(3*resultadoGauss[1])+(-1*resultadoGauss[2])+(-2*resultadoGauss[3])
+    console.log("Solucion Ec4 = " + valor)
+*/
+}
+
+);
 
 
 
@@ -329,38 +467,5 @@ if (matrizDominante()) {
 
 
 
-
-console.log("============ METODO JACOBI =============")
-//resultadoJacobi = jacobi();
-//console.log(resultadoJacobi)
-
-valor = (-3*resultadoJacobi[0])+(9*resultadoJacobi[1])+(-1*resultadoJacobi[2])+(3*resultadoJacobi[3])
-console.log("Solucion Ec1 = " + valor)
-
-valor = (2*resultadoJacobi[0])+(-1*resultadoJacobi[1])+(13*resultadoJacobi[2])+(-1*resultadoJacobi[3])
-console.log("Solucion Ec2 = " + valor)
-
-valor = (5*resultadoJacobi[0])+(-1*resultadoJacobi[1])+(2*resultadoJacobi[2])+(8*resultadoJacobi[3])
-console.log("Solucion Ec3 = " + valor)
-
-valor = (6*resultadoJacobi[0])+(3*resultadoJacobi[1])+(-1*resultadoJacobi[2])+(-2*resultadoJacobi[3])
-console.log("Solucion Ec4 = " + valor)
-
-
-console.log("============ METODO GAUSS-SEIDEL ==============")
-//resultadoGauss = gaussSeidel();
-//console.log(resultadoGauss)
-
-valor = (-3*resultadoGauss[0])+(9*resultadoGauss[1])+(-1*resultadoGauss[2])+(3*resultadoGauss[3])
-console.log("Solucion Ec1 = " + valor)
-
-valor = (2*resultadoGauss[0])+(-1*resultadoGauss[1])+(13*resultadoGauss[2])+(-1*resultadoGauss[3])
-console.log("Solucion Ec2 = " + valor)
-
-valor = (5*resultadoGauss[0])+(-1*resultadoGauss[1])+(2*resultadoGauss[2])+(8*resultadoGauss[3])
-console.log("Solucion Ec3 = " + valor)
-
-valor = (6*resultadoGauss[0])+(3*resultadoGauss[1])+(-1*resultadoGauss[2])+(-2*resultadoGauss[3])
-console.log("Solucion Ec4 = " + valor)
 
 
